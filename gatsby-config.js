9bcce85f167d9121ffbc2093d089612dfe2bc69b
Ramon Lence <rlence86@gmail.com>
2019-12-11 22:24:16 +0100

    added pwa data

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: 'myAvatar.png' // This path is relative to the root of the site.
+        icon: 'static/myAvatar.png' // This path is relative to the root of the site.
       }
     },
     {
