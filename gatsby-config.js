92c46d5265ea49b8c5ba772d32fdf997ccea822c
Ramon Lence <rlence86@gmail.com>
2019-12-11 22:22:54 +0100

    added pwa data

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'GatsbyJS',
-        short_name: 'GatsbyJS',
+        name: 'RamonLence',
+        short_name: 'RamonLence',
         start_url: '/',
         background_color: '#6b37bf',
         theme_color: '#6b37bf',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone'
-        // icon: 'public/favicon.png' // This path is relative to the root of the site.
+        display: 'standalone',
+        icon: 'myAvatar.png' // This path is relative to the root of the site.
       }
     },
     {
